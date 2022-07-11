2a88e1adcdc1747bc35d99d94554333310e3c0ad
Biswa Bhusan Gupta <66106600+biswa-bhusan-gupta@users.noreply.github.com>
2022-07-11 12:24:07 +0530

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { TestComponent } from "./test/test.component";
 import { CardComponent } from "./card/card.component";
+import { PageComponent } from "./page/page.component";
 
 const routes: Routes = [
+  { path: "", redirectTo: "/card", pathMatch: "full" },
   { path: "test", component: TestComponent },
-  { path: "card", component: CardComponent }
+  { path: "card", component: CardComponent },
+  { path: "**", component: PageComponent }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
-export const routingComponents = [TestComponent, CardComponent];
+export const routingComponents = [TestComponent, CardComponent, PageComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 // import { TestComponent } from "./test/test.component";
 // import { CardComponent } from "./card/card.component";
+import { PageComponent } from "./page/page.component";
+
 import { AppRoutingModule, routingComponents } from "./app-routing.module";
 
 @NgModule({
-  declarations: [AppComponent, routingComponents],
+  declarations: [AppComponent, routingComponents, PageComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
